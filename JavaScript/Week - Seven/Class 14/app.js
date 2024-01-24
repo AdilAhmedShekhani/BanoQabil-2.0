96945700ce927ead9defe443dd4c78e58ec79fb0
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-01-24 11:39:00 +0500

    alert and console  Variable  Syntax of JS

---
+++ JavaScript/Week - Seven/Class 14/app.js
@@ -0,0 +1,84 @@
+// Syntax of Js (how to write in Javascript)
+
+//  Alert Method (Pre Defined JS Method)
+// alert("Hello Students");
+
+// window.alert()
+
+// Data Types
+
+// Numbers
+// 123456789;
+
+// Boolean
+// true;
+// false;
+
+// String
+// "abcdef";
+// "abc123";
+// "1213456";
+// "true";
+// "false";
+
+// Errors and code are shown in Console only (f12)
+
+// Variable Create (One or more times usage of value in js)
+
+// myFullName (Camel Case)
+// my_full_name (Snake Case)
+
+// Variable (String)
+
+// var fullName = "Adil Ahmed";
+
+// alert(fullName);
+// alert(fullName);
+// alert(fullName);
+// alert(fullName);
+
+// Update Variable
+
+// var fullName = "Ali";
+
+// alert(fullName);
+
+// var fullName = "Adil Ahmed";
+
+// alert(fullName);
+
+// Undefined Variable
+
+// var fullName;
+
+// Variable (Numbers)
+
+// var num1 = 10;
+
+// alert(num1);
+
+// alert(num1 + 10);
+
+// var num1 = 10;
+// var num2 = 10;
+
+// alert(num1 + num2);
+
+// Print Value in Console
+// var Name = "Adil Ahmed";
+
+// console.log(Name);
+
+// Math Operators
+
+var num = 10;
+
+console.log(num + 5); //Adition
+console.log(num * 5); //Multiply
+console.log(num / 2); // Divide
+console.log(num % 2); // Reminder
+
+// POST INCREMENT AND PRE INCREMENT
+
+console.log(++num);
+console.log(num++);
