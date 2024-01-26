7d616aba7f449ac650fcc7106be806145ffcba13
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-01-26 11:49:26 +0500

    Prompt and Statements (if and else

---
+++ JavaScript/Week - Eight/Class 15/app.js
@@ -0,0 +1,100 @@
+// Connect JavaScript
+
+// alert("Hello Students");
+
+//Type of Variable
+
+// var num = true;
+// console.log(typeof num);
+
+// Prompt
+
+// var userName = prompt();
+// var userName = prompt("Please Enter Your User Name");
+
+// console.log(userName);
+
+// if statements (true or false)
+
+// if (true) {
+//   alert("Good");
+// }
+
+// var city = prompt("The city of lights ?");
+// if (city === "karachi") {
+//   alert("Correct");
+// }
+
+// var gender = prompt("Please Enter Your gender");
+// if (gender === "Male") {
+//   alert("Welcome Sir");
+// }
+
+// if (gender === "Female") {
+//   alert("Welcome Maam");
+// }
+
+// var gender = prompt("Please Enter Your gender");
+// var message = "Please Enter Correct one";
+// if (gender === "Male") {
+//   message = "Welcome Sir";
+// }
+
+// if (gender === "Female") {
+//   message = "Welcome Maam";
+// }
+
+// alert(message);
+
+//  Comparision Operators
+
+// > is greater than
+// < is less than
+// >= is greater than or equal to
+// > is less than or equal to
+
+// var age = 21;
+
+// if (age <= 20) {
+//   console.log("Eligible");
+// }
+
+// if else and else if statements
+
+// var age = 22;
+
+// if (age <= 20) {
+//   console.log("Eligible");
+// } else {
+//   console.log("Not Eligible");
+// }
+
+// var kuchLanahai = prompt("Kia mil raha hai ");
+
+// if (kuchLanahai === "Dahi") {
+//   console.log("le ana");
+// } else if (kuchLanahai === "Dhoodh") {
+//   console.log("Acha Le aao");
+// } else if (kuchLanahai === "Bread") {
+//   console.log("bhai le aao yar");
+// } else {
+//   console.log("Bhai kuch bhi le aao");
+// }
+
+// var language = prompt("Enter Your Language");
+
+// if (language === "javascript") {
+//   var city = prompt("Enter your city");
+//   if (city === "karachi") {
+//     var exp = prompt("Enter your experience");
+//     if (exp === "2 years") {
+//       console.log("Congratulation, you are Hired");
+//     } else {
+//       console.log("Barhe ho jao");
+//     }
+//   } else {
+//     console.log("Karachi Aaja bhai");
+//   }
+// } else {
+//   console.log("JavaScript Seekho Beta");
+// }
