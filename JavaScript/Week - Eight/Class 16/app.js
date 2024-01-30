1e15077697677ebbed857c0c6d4b37d954e26a42
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-01-30 11:47:43 +0500

    Arrays

---
+++ JavaScript/Week - Eight/Class 16/app.js
@@ -0,0 +1,30 @@
+// alert("Hello Students");
+
+// Arrays
+
+var student1 = "Noman";
+var student2 = "Ahmed";
+
+var students = ["Noman", "Ahmed", "Ali"];
+
+console.log(student1);
+
+console.log(students);
+
+// Index of Array (Start from 0)
+
+console.log(students[1]); // Get Particular value from Array
+
+// Data Type Store in Variable (All Data types)
+
+// Array Adding and Remove
+
+// Add in Arrays
+students[3] = "Adil";
+students[4] = "Abc";
+
+var students = [];
+students[students.length] = "Ahmed";
+students[students.length] = "Ali";
+
+console.log(students);
