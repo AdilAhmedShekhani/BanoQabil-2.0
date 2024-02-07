1c8668f413ce2db7ccf707787fb2d0532c20adb7
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-02-07 14:18:25 +0500

    Functions & Loops

---
+++ JavaScript/Week - Nine/Class 17/app.js
@@ -0,0 +1,90 @@
+// console.log("Hello Students");
+
+//Methods
+// alert()
+// prompt()
+
+// Create Function
+
+// Normal Function
+
+// function abc() {
+//   var abc = "name";
+//   console.log("abc==>", abc);
+// }
+
+// abc() // Function Call
+
+// Create Function
+
+// Arrow Function (ES-6)
+
+// const abc = () => {
+//   var abc = "name";
+//   console.log("abc==>", abc);
+// };
+
+// abc(); // Function Call
+
+// Create loops
+
+// For Loop
+
+// for(statement, condition, increment){
+//     //Block
+// }
+
+//statement condition increment
+// for (var i = 1; i <= 100; i++) {
+//   console.log("Loop==>", i);
+// }
+
+// var text = "Hello Students";
+
+// for (var i = 1; i <= 5; i++) {
+//   console.log("Loop==>", i, text);
+// }
+
+// var cities = ["karachi", "hyderabad", "Sukkur", "Multan", "Islamabad"];
+// for (var i = 0; i < cities.length ; i++) {
+//   console.log("Cities==>", cities[i]);
+// }
+
+// Table Print
+// var table = 2; //static value
+// for (var i = 1; i <= 10; i++) {
+//   console.log("Table ==>", table + "X" + i + "=" + table * i);
+// }
+
+// Flag
+
+// var cities = ["karachi", "hyderabad", "Sukkur", "Multan", "Islamabad"];
+// var flag = false;
+// for (var i = 0; i < cities.length; i++) {
+//   if (cities[i] == "Multan") {
+//     flag = true;
+//   }
+// }
+
+// if (flag) {
+//   console.log("Found");
+// } else {
+//   console.log("Not Found");
+// }
+
+// Break
+
+// for (var i = 0; i < 10; i++) {
+//   console.log(i);
+//   if (i === 4) {
+//     break;
+//   }
+// }
+
+// Palindrome
+
+// civic;
+// madam;
+// radar;
+// level;
+// refer;
