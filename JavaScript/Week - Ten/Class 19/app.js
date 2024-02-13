0e9a529dcd4f1c83aa75449a7516ed27dd86f1c6
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-02-13 16:43:13 +0500

    Date Method

---
+++ JavaScript/Week - Ten/Class 19/app.js
@@ -0,0 +1,18 @@
+alert("Hello Students");
+
+// Date Methods
+
+console.log(new Date("2000 01 12"));
+
+var date = new Date("29 june, 1980");
+var dayNames = [
+  "اتوار",
+  "پیر",
+  "منگل",
+  "بدھ",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+console.log(dayNames[date.getDay()]);
