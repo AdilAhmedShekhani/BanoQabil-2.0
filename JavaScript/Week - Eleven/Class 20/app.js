a512eb14a28ce792195fad1aadcf897eb2de716c
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-02-19 12:58:58 +0500

    CallBacks

--- JavaScript/Week - Eleven/Class 20/app.js
+++ JavaScript/Week - Eleven/Class 20/app.js
@@ -77,6 +77,25 @@ promise
     console.log("Some error has occurred");
   });
 
+// Callback
 
-  
+// The add() function is
+// called with arguments a, b
+// and callback, callback
+// will be executed just
+// after ending of add() function
+function add(a, b, callback) {
+  console.log(`The sum of ${a} 
+  and ${b} is ${a + b}`);
+  callback();
+}
 
+// The disp() function is called just
+// after the ending of add() function
+function disp() {
+  console.log(`This must be printed
+   after addition`);
+}
+
+// Calling add() function
+add(5, 6, disp);
