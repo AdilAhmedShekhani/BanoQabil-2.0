c9892e62c30c58d68ad3c6d5724aaa089753809b
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-02-20 16:36:32 +0500

    Closures

---
+++ JavaScript/Week - Eleven/Class 21/app.js
@@ -0,0 +1,28 @@
+//Closures
+
+function makeAdder(x) {
+  return function (y) {
+    return x + y;
+  };
+}
+
+const add5 = makeAdder(5);
+const add10 = makeAdder(10);
+
+console.log(add5(2)); // 7
+console.log(add10(2)); // 12
+
+//Practical Example of Closures
+// function makeSizer(size) {
+//   return function () {
+//     document.body.style.fontSize = `${size}px`;
+//   };
+// }
+
+// const size12 = makeSizer(24);
+// const size14 = makeSizer(48);
+// const size16 = makeSizer(72);
+
+// document.getElementById("size-12").onclick = size12;
+// document.getElementById("size-14").onclick = size14;
+// document.getElementById("size-16").onclick = size16;
