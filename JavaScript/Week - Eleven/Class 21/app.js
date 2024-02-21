159f93296892ce0aca57de30df9567dd46abe45a
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-02-21 11:38:14 +0500

    Fetch method

--- JavaScript/Week - Eleven/Class 21/app.js
+++ JavaScript/Week - Eleven/Class 21/app.js
@@ -1,16 +1,34 @@
-//Closures
+// JSON Formater Extension
 
-function makeAdder(x) {
-  return function (y) {
-    return x + y;
-  };
+//Fetch Method (Get Data from API)
+
+let output = document.getElementById("output");
+function fetchData() {
+  fetch("https://dummyjson.com/products/1")
+    .then((response) => response.json())
+    .then((data) => {
+      output.innerHTML += "id = " + data.id + "<br/>";
+      output.innerHTML += "brand = " + data.brand + "<br/>";
+      output.innerHTML += "category = " + data.category + "<br/>";
+      output.innerHTML += "price = " + data.price + "<br/>";
+      output.innerHTML += "rating = " + data.rating + "<br/>";
+      output.innerHTML += "stock = " + data.stock + "<br/>";
+    });
 }
 
-const add5 = makeAdder(5);
-const add10 = makeAdder(10);
+//Closures
 
-console.log(add5(2)); // 7
-console.log(add10(2)); // 12
+// function makeAdder(x) {
+//   return function (y) {
+//     return x + y;
+//   };
+// }
+
+// const add5 = makeAdder(5);
+// const add10 = makeAdder(10);
+
+// console.log(add5(2)); // 7
+// console.log(add10(2)); // 12
 
 //Practical Example of Closures
 // function makeSizer(size) {
